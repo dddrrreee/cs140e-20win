ff384e15376bfeb6af45adf0b6090c50dfb1f5f8
engler <ddd.rrr.eee@gmail.com>
2020-01-19 23:19:05 -0800

    added (hopefully better) makefiles.

---
+++ support/mk/template.mk
@@ -0,0 +1,46 @@
+#  a template that should work in all the different directories.
+
+# we've had some make issues going cross windows/mac/linux, so we just do brute stupid
+# to make things work: no fancy rules, just always recompile.
+NAME := hello
+
+# check that your path is defined.
+ifndef CS140E_PATH
+$(error CS140E_PATH is not set: this should contain the absolute path to where this directory is.  Define it in your shell's initialiation.  For example, .tcshrc for tcsh or .bashrc for bash)
+endif
+
+include $(CS140E_PATH)/libpi/includes.mk
+
+# if you want to compile everything in a directory, do something like this.
+SOURCES := $(wildcard ./*.c)
+OBJS := $(patsubst %.c, %.o, $(SOURCES))
+# OBJS := $(NAME).o
+
+# just say that everything depends on all the .h's.
+DEPS := $(wildcard ./*.h) $(LPI)
+DEPS := $(DEPS) $(LPI) ./Makefile 
+
+all: libpi $(OBJS) $(NAME).bin
+
+libpi: 
+	make -C $(LPP)
+
+$(OBJS): $(DEPS)
+
+# <LPI> will get rebuilt if anything changes, so we don't need any other
+# dependencies.
+$(NAME).bin: $(MEMMAP) $(OBJS) $(LPI) ./Makefile
+	$(LD) $(OBJS) -T $(LPP)/memmap -o $(NAME).elf  $(LPI)
+	$(OD) -D $(NAME).elf > $(NAME).list
+	$(OCP) $(NAME).elf -O binary $(NAME).bin
+
+run:
+	my-install $(NAME).bin
+
+tags: 
+	ctags *.[chSs] */*.[chSs]
+
+clean:
+	rm -rf $(BUILD_DIR) $(NAME) *~ tags *.o *.bin *.elf *.list *.img Makefile.bak 
+
+.PHONY: libpi all run tags clean 

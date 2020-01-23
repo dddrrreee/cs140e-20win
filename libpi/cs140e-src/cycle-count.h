c8d34d2fb65479be022f542ec1df4c1e07d771d1
engler <ddd.rrr.eee@gmail.com>
2020-01-23 13:38:21 -0800

    minor

---
+++ libpi/cs140e-src/cycle-count.h
@@ -0,0 +1,17 @@
+#ifndef __CYCLE_COUNT_H__
+// use r/pi cycle counters so that we can do tighter timings.
+
+// must do init first.
+#define cycle_cnt_init() do {                                   \
+    unsigned in = 1;                                            \
+    asm volatile("MCR p15, 0, %0, c15, c12, 0" :: "r"(in));     \
+} while(0)
+
+// read.  should add a write().
+#define read_cycle_cnt() ({	 					\
+	unsigned _out;							\
+  	asm volatile ("MRC p15, 0, %0, c15, c12, 1" : "=r"(_out));	\
+	_out;								\
+})
+
+#endif

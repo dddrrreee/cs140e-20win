52059a4a086fa647b762fce0e5145201d0cef514
engler <ddd.rrr.eee@gmail.com>
2020-01-29 08:40:32 -0800

    bug fixes.

--- labs/6-threads/1-kmalloc/test-kmalloc.c
+++ labs/6-threads/1-kmalloc/test-kmalloc.c
@@ -7,10 +7,12 @@ void notmain() {
     printk("starting test\n");
     kmalloc_init();
 
+    printk("heap starts at 0x%x\n", kmalloc_heap_ptr());
 
     void *heap0 = kmalloc_heap_ptr();
     // sleazy check that assumes how kmalloc works.
     unsigned *start = kmalloc(1);
+    printk("start=0x%x\n", start);
     for(unsigned i = 0; i < 20; i++) {
         unsigned *u = kmalloc(1);
         unsigned v = (i << 24 | i << 16 | i << 8 | i);
@@ -20,8 +22,8 @@ void notmain() {
     unsigned *u = start+1;
     for(unsigned i = 0; i < 20; i++) {
         unsigned v = (i << 24 | i << 16 | i << 8 | i);
-        demand(start[i] == v, 
-            "invalid start[%d] =%x, should be %x\n", start[i],v);
+        demand(u[i] == v, 
+            "invalid start[%d] =%x, should be %x\n", u[i],v);
     }
 
     void *p = kmalloc_aligned(1,64);
@@ -33,8 +35,5 @@ void notmain() {
     demand(start == kmalloc(1), did not reset the heap correctly!);
 
     printk("successful test\n");
-    printk("going to revoke kmalloc_init: should panic now\n");
-    kmalloc_init();
-    panic("test failed: should not reach here\n");
 	clean_reboot();
 }

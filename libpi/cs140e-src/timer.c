8720c12093d24b91318697fa78d1d4ecaf49a39c
engler <ddd.rrr.eee@gmail.com>
2020-04-30 18:26:18 -0700

    fixed delay

--- libpi/cs140e-src/timer.c
+++ libpi/cs140e-src/timer.c
@@ -3,8 +3,14 @@
 // roughly 2-3x the number of cyles.  dunno if we care.  can read cycle count from
 // cp15 iirc.
 void delay_cycles(unsigned ticks) {
+#ifndef RPI_UNIX
     while(ticks-- > 0)
         asm("add r1, r1, #0");
+#else
+    void dummy_fn(void);
+    while(ticks-- > 0)
+        dummy_fn();
+#endif
 }
 
 // no dev barrier.
